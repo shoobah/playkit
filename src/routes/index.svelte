8810bc59b48e4bcbba2b8cd4db22245504a51893
shoobah <shoobah@gmail.com>
2021-06-27 01:42:34 +0200

    Display values

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,9 @@
 
 	let toggles = [];
 
-function getRandom() {
-  return Math.floor(Math.random() * 2);
-}
+	function getRandom() {
+		return !!Math.floor(Math.random() * 2);
+	}
 
 	onMount(() => {
 		for (let i = 0; i < 10; i++) {
@@ -19,16 +19,23 @@ function getRandom() {
 			];
 		}
 	});
+
+	function onStateChange(s){
+		const t = toggles.find(i=>i.id===s.detail.id)
+		t.value = s.detail.value;
+		toggles=toggles;
+	}
 </script>
 
 {#each toggles as t}
-    <div style="padding:0; margin:0;">
-        <div style="width:100px; display:inline-block">{t.id}:</div> <Toggle id={t.id} value={t.value} duration="200" />
-    </div>
+	<div style="padding:0; margin:0;">
+		<div style="width:200px; display:inline-block">{t.id} - {t.value}:</div>
+		<Toggle id={t.id} value={t.value} duration="200" on:state={onStateChange} />
+	</div>
 {/each}
 
 <style>
-    div{
-        font-family: Arial, Helvetica, sans-serif;
-    }
+	div {
+		font-family: Arial, Helvetica, sans-serif;
+	}
 </style>
