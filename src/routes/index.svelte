6ba46f7fb520c1cf0e1d5b5edc13fbbbe03e69eb
shoobah <shoobah@gmail.com>
2021-07-05 17:48:30 +0200

    Fiddling with tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,12 +27,14 @@
 	}
 </script>
 
-{#each toggles as t}
-	<div style="padding:0; margin:0;">
+<div class="grid md:grid-cols-3 gap-4 sm:grid-cols-1">
+	{#each toggles as t}
+	<div class="justify-self-center bg-pink-300 rounded-lg p-2">
 		<div style="width:200px; display:inline-block">{t.id} - {t.value}:</div>
 		<Toggle id={t.id} value={t.value} duration="200" on:state={onStateChange} />
 	</div>
-{/each}
+	{/each}
+</div>
 
 <style>
 	div {
